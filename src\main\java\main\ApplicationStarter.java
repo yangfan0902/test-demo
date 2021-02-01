a5f39f3cdb1b88a95a4dadbec89890543270465a
yangfan <42598769+yangfan0902@users.noreply.github.com>
2021-02-01 13:17:16 +0800

    Update src\main\java\main\ApplicationStarter.java

--- src\main\java\main\ApplicationStarter.java
+++ src\main\java\main\ApplicationStarter.java
@@ -1,18 +1,24 @@
 package main;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-@EnableRetry
 public class ApplicationStarter {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName, @Value("${spring.project.name}") String projectName) {
+        return (registry) -> registry.config().commonTags("application",applicationName,"app", applicationName,"project",projectName);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
     }
