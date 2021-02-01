e90ad5ae87e73fac16b3fdd2d71259dac1241e03
yangfan <42598769+yangfan0902@users.noreply.github.com>
2021-02-01 13:43:01 +0800

    Update src\main\java\main\ApplicationStarter.java

--- src\main\java\main\ApplicationStarter.java
+++ src\main\java\main\ApplicationStarter.java
@@ -1,11 +1,8 @@
 package main;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
