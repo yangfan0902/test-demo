d2c02c6a7c6eb77b261b01517830c0a5cd34ba1d
yangfan <42598769+yangfan0902@users.noreply.github.com>
2021-02-01 13:19:30 +0800

    Update src\main\java\main\Controller.java

--- src\main\java\main\Controller.java
+++ src\main\java\main\Controller.java
@@ -1,63 +1,117 @@
 package main;
 
-import com.envisioniot.enos.ecp.harbor.client.HarborClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.scheduling.annotation.Async;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.Resource;
-import javax.xml.bind.SchemaOutputResolver;
-import java.io.IOException;
-import java.net.SocketTimeoutException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @RestController
 public class Controller {
-
-    @Autowired
-    RetryService retryService;
+    @Value ("${spring.application.name}") private String appName;
 
 
-
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-
-    @RequestMapping("/test1")
-        public void index() {
-            try {
-                Object o = retryService.testdeploy();
-                if(false){
-
-                }else {
-                    System.out.println("test if");
-                }
-            }catch (ResourceAccessException e){
-                if (e.getCause() instanceof SocketTimeoutException) {
-                    System.out.println("Request timed out");
-                }
-                System.out.println("socket time out");
-            }
-            catch (Exception e){
-                System.out.println(e.getMessage());
-                System.out.println(e.getMessage().contains("Read timed out"));
-                System.out.println(e.getClass());
-            }
-
+    @GetMapping("/")
+    public String index() {
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return "this is " + appName;
+    }
 
-        @RequestMapping("/")
-        public String index1() {
-            return "hello"
-            System.out.println("end");
-        }
+//    @RequestMapping("/deploy/success")
+//    public String deploySuccess() {
+//        monitor.getSuccessDeployCount().increment();
+//        Random random = new Random();
+//        int amount = random.nextInt(100);
+//        monitor.getDeployTimeSum().record(amount);
+//        return "部署成功";
+//
+//    }
+
+//    @RequestMapping("/deploy/failed")
+//    public String deployFailed() {
+//        monitor.getFailedDeployCount().increment();
+//        return "部署失败";
+//
+//    }
+
+//    @RequestMapping("/deploy/success/app-a")
+//    public String deploySuccess() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getSuccessDeployCount("op", "app-a", time,"deploy", "alpha").increment();
+//        Random random = new Random();
+//        int amount = random.nextInt(100);
+//        monitor.getDeployTimeSum("op", "app-a", time,"deploy", "alpha").record(amount);
+//        return "部署成功";
+//
+//    }
+//
+//    @RequestMapping("/deploy/success/app-b")
+//    public String deploySuccess1() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getSuccessDeployCount("op", "app-b", time,"deploy", "beta").increment();
+//        Random random = new Random();
+//        int amount = random.nextInt(100);
+//        monitor.getDeployTimeSum("op", "app-b", time,"deploy", "beta").record(amount);
+//        return "部署成功";
+//
+//    }
+//
+//    @RequestMapping("/deploy/success/app-c")
+//    public String deploySuccess2() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getSuccessDeployCount("enos", "app-c", time,"build", "alpha").increment();
+//        Random random = new Random();
+//        int amount = random.nextInt(100);
+//        monitor.getDeployTimeSum("enos", "app-c", time,"build", "alpha").record(amount);
+//        return "部署成功";
+//
+//    }
+//
+//    @RequestMapping("/deploy/failed/app-c")
+//    public String deployfailed() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getFailedDeployCount("enos", "app-c", time,"build", "alpha").increment();
+//        return "部署失败";
+//
+//    }
+//
+//    @RequestMapping("/deploy/failed/app-a")
+//    public String deployfailed1() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getFailedDeployCount("op", "app-a", time,"deploy", "alpha").increment();
+//        return "部署失败";
+//
+//    }
+//    @RequestMapping("/deploy/failed/app-b")
+//    public String deployfailed2() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getFailedDeployCount("op", "app-b", time,"deploy", "beta").increment();
+//        return "部署失败";
+//
+//    }
+//
+//    @RequestMapping("/deploy/success/app-a/beta")
+//    public String deploySuccess4() {
+//        Long time = System.currentTimeMillis();
+//        monitor.getSuccessDeployCount("op", "app-a", time,"deploy", "beta").increment();
+//        Random random = new Random();
+//        int amount = random.nextInt(100);
+//        monitor.getDeployTimeSum("op", "app-a", time,"deploy", "beta").record(amount);
+//        return "部署成功";
+//
+//    }
+
+    @RequestMapping("/hello")
+    public String clear() {
+        return "hello world";
+    }
+
+
+
+
+
 }
